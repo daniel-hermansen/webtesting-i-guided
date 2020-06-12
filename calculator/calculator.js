dbf60a7b929a68a0dab1f608eb2f18dc85074b75
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-06-11 19:22:39 -0600

    Completed guided project.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,21 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+
+function add(a = 0, b = 0) {
+  if(isNumber(a) && isNumber(b)){
+    return a + b;
+  } else {
+    throw new Error('Both arguments need to be numbers');
+  }
 }
+
+function isNumber(value) {
+  return !isNaN(parseFloat(value)) && isFinite(value);
+}
+// Start testing with "low hanging fruit" --> happy path (your basic assumptions), pure functions, etc.
+// Check for invalid input
+// Then move to edge cases
+
+// Automated testing is better because it is unbiased and can catch errors that the author of the code might not catch. 
+// Automated testing will also provide a safety net against regressions (when working code suddenly stops working) 
